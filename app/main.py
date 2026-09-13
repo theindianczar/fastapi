fbbb22ce4442b4c18f29a73d218f18548d353f22
theindianczar <theindianczar@gmail.com>
2026-09-13 21:38:34 +0530

    adding first github actions

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
 from .routers import auth, post, user, vote
-from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
